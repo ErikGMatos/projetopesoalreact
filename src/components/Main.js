420821c58a0c2192d3a5a268a6feba014809bbc4
Erik Matos <erik_senac@hotmail.com>
2019-02-11 22:06:40 -0200

    alterando arquivos

---
+++ src/components/Main.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Main = props => {
+    return (
+        <div>
+            <p>Aqui é o corpo do conteúdo</p>
+        </div>
+    );
+};
+
+export default Main;
---
+++ src/components/SideBar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Sidebar = props => {
+    return (
+        <div>
+            <p>Aqui é o menu lateral</p>
+        </div>
+    );
+};
+
+export default Sidebar;
