e111f81e31b324436479e3e395cdd6d24e8e4f33
Erik Matos <erik_senac@hotmail.com>
2019-02-27 09:21:50 -0300

    alterando dados no grafico

--- src/components/GraphComponent/bar.js
+++ src/components/GraphComponent/bar.js
@@ -22,19 +22,31 @@ class BarGraph extends Component {
                         borderColor: "rgba(255,99,132,1)",
                         borderWidth: 1,
                         hoverBackgroundColor: "rgba(255,99,132,0.4)",
-                        hoverBorderColor: "rgba(255,99,132,1)",
-                        data: [1.5, 2, 3, 3, 5, 8, 2, 9, 1, 6, 4, 5]
+                        hoverBorderColor: "rgba(255,99,132,1)"
+                        //data: [1.5, 2, 3, 3, 5, 8, 2, 9, 1, 6, 4, 5]
                     }
                 ]
             }
         };
     }
 
-    componentWillMount(){
-        
-        if(this.props.data){
-            //this.setState({meusDados.datasets[0].data = [1,2,3]})
+    componentWillMount() {
+        const novoEstado = Object.assign({}, this.state.meusDados, {
+            labels: [...this.state.meusDados.labels],
+            datasets: [
+                {
+                    ...this.state.meusDados.datasets[0],
+                    data: [50, 40, 30, 20, 19, 90, 60]
+                }
+            ]
+        });
+        if (!this.props.data) {
+            this.setState({
+                meusDados: novoEstado
+            });
         }
+        console.log(this.state.meusDados);
+        console.log(novoEstado);
     }
 
     render() {
--- src/components/Main.js
+++ src/components/Main.js
@@ -7,28 +7,28 @@ import Bar from "./GraphComponent/bar";
 import Line from "./GraphComponent/line";
 
 
-const dataBar = {
-    labels: [
-        "Janeiro",
-        "Fevereiro",
-        "Março",
-        "Abril",
-        "Maio",
-        "Junho",
-        "Julho"
-    ],
-    datasets: [
-        {
-            label: "Erik",
-            backgroundColor: "rgba(255,99,132,0.2)",
-            borderColor: "rgba(255,99,132,1)",
-            borderWidth: 1,
-            hoverBackgroundColor: "rgba(255,99,132,0.4)",
-            hoverBorderColor: "rgba(255,99,132,1)",
-            data: [1.5, 2, 3, 3, 5, 8, 2, 9, 1, 6, 4, 5]
-        }
-    ]
-};
+// const dataBar = {
+//     labels: [
+//         "Janeiro",
+//         "Fevereiro",
+//         "Março",
+//         "Abril",
+//         "Maio",
+//         "Junho",
+//         "Julho"
+//     ],
+//     datasets: [
+//         {
+//             label: "Erik",
+//             backgroundColor: "rgba(255,99,132,0.2)",
+//             borderColor: "rgba(255,99,132,1)",
+//             borderWidth: 1,
+//             hoverBackgroundColor: "rgba(255,99,132,0.4)",
+//             hoverBorderColor: "rgba(255,99,132,1)",
+//             data: [1.5, 2, 3, 3, 5, 8, 2, 9, 1, 6, 4, 5]
+//         }
+//     ]
+// };
 
 const dataLine = {
     labels: ["January", "February", "March", "April", "May", "June", "July"],
@@ -61,7 +61,7 @@ class Main extends Component {
         return (
             <Row>
                 <Col s={6}>
-                    <Bar data={dataBar} />
+                    <Bar  />
                 </Col>
                 <Col s={6}>
                     <Line data={dataLine} />
